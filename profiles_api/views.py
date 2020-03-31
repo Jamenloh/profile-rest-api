830c3a87608ff15f3b74dd50535a79a217cef75c
Jamen <jamen@djangogirls.co>
2020-03-31 18:06:58 +0800

    added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+
+        an_apiview = [
+            'View 1',
+            'View 2',
+            'View 3',
+            'View 4',
+        ]
+
+        return Response({'message':'Hello!', 'an_apiview': an_apiview})
